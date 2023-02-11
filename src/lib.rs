8066f95d717c4c704a171eb6e08efb13c7929b02
Cindy Xiao <git@cxiao.net>
2023-02-11 12:44:40 -0800

    feat: Add basic plugin registration code

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,10 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use log::{LevelFilter, debug};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[no_mangle]
+#[allow(non_snake_case)]
+pub extern "C" fn CorePluginInit() -> bool {
+    binaryninja::logger::init(LevelFilter::Trace).expect("failed to initialize logging");
+    
+    debug!("Initializing binary view");
+    true
 }
